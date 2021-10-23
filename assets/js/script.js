21326350ae145a08dda52fea7538795ef7b39cb0
Atijosan-babatunde <babatundeatijosan99@gmail.com>
2021-10-23 08:38:57 +0100

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,17 @@
 function myFunction() {
     var name = document.getElementById("name").value;
     var text = document.getElementById("text").value;
-    var inputGrade = document.getElementById("inputGrade").value;
+    var foodOrdered = document.getElementById("foodOrdered").value;
+    var foodquality = document.getElementById("foodquality").value;
+    var orderaccuracy = document.getElementById("orderaccuracy").value;
+    var valueformoney = document.getElementById("valueformoney").value;
+    var speedofservice = document.getElementById("speedofservice").value;
+    var overallexperience = document.getElementById("overallexperience").value;
+    var serverName = document.getElementById("serverName").value;
+    var additionalComments = document.getElementById("additionalComments").value;
     // Returns successful data submission message when the entered information is stored in database.
-    var dataString = 'name1=' + name + '&text1=' + text + '&inputGrade1=' + inputGrade; 
-    if (name == '' || text == '' || inputGrade == '') {
+    var dataString = 'name1=' + name + '&text1=' + text + '&foodOrdered1=' + foodOrdered + '&foodquality1=' + foodquality + '&orderaccuracy1=' + orderaccuracy + '&valueformoney1=' + valueformoney + '&speedofservice1=' + speedofservice + '&overallexperience1=' + overallexperience + '&serverName1=' + serverName + '&additionalComments' + additionalComments +; 
+    if (name == '' || text == '' || foodOrdered == '' || foodquality == '' || orderaccuracy == '' || valueformoney == '' || speedofservice == '' || overallexperience == '' || serverName == '' || additionalComments == '' ||) {
     alert("Please Fill All Fields");
     } else {
     // AJAX code to submit form.
--- assets/js/userInfo.php
+++ assets/js/userInfo.php
@@ -4,13 +4,26 @@ include('db.php');
 
 if(isset($_POST['submit']))
 {
+
+    $msg="Name: ".$_POST['name']."\n"."Food Order: ".$_POST['foodOrdered']."\n"."Food Quality: ".$_POST['foodquality']."\n"."Order Accuracy: ".$_POST['orderaccuracy']."\n"."Value For Money: ".$_POST['valueformoney']. "\n"."speed of service: ".$_POST['speedofservice']. "\n"."over all experience:".$_POST['overallexperience']. "\n"."server name:".$_POST['serverName']."\n"."additional comments:".$_POST['additionalComments'];
+
+// use wordwrap() if lines are longer than 70 characters
+     $msg = wordwrap($msg,70);
+
+// send email
+     mail("mabonks@yahoo.com","My subject",$msg);
+
     $name = $_POST['name'];
     $foodOrdered = $_POST['foodOrdered'];
-    $inputGrade = $_POST['inputGrade'];
+    $foodquality = $_POST['foodquality'];
+    $orderaccuracy = $_POST['orderaccuracy'];
+    $valueformoney = $_POST['valueformoney'];
+    $speedofservice = $_POST['speedofservice'];
+    $overallexperience = $_POST['overallexperience'];
     $serverName = $_POST['serverName'];
     $additionalComments = $_POST['additionalComments'];
 
-    $sql = mysqli_query($conn, "INSERT INTO user (name,foodOrdered,inputGrade,serverName,additionalComments) VALUES('$name','$foodOrdered','$inputGrade','$serverName','$additionalComments')");
+    $sql = mysqli_query($conn, "INSERT INTO user (name,foodOrdered,foodquality,orderaccuracy,valueformoney,speedofservice,overallexperience,serverName,additionalComments) VALUES('$name','$foodOrdered','$foodquality','$orderaccuracy','$valueformoney','$speedofservice','$overallexperience','$serverName','$additionalComments')");
     if($sql)
     {
         echo '<script>'; 
@@ -25,3 +38,11 @@ echo '</script>';
     }
 }
 ?>
+
+
+
+
+
+
+
+
